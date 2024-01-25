9198be1539417d5192303829468ea1d4facb7fa1
allaboutevemirolive <xkmxlfirdxus@gmail.com>
2024-01-25 09:28:34 +0200

    initial_commit

---
+++ fetcher/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use reqwest;
+use std::io::Write;
+
+fn main() {
+    let url = "https://www.vpngate.net/api/iphone/";
+
+    let response = reqwest::blocking::get(url);
+
+    match response {
+        Ok(res) => {
+            if res.status().is_success() {
+                // Read the response body as bytes
+                let bytes = res.bytes().expect("Failed to read response body");
+
+                // Specify the filename to save the CSV data
+                let filename = "/home/nemesis/Documents/Github/Focus/network/origami/data/vpngate_data.csv";
+
+                // Write the bytes to a file
+                let mut file = File::create(filename).expect("Failed to create file");
+                file.write_all(&bytes).expect("Failed to write to file");
+
+                println!("CSV data downloaded and saved to {}", filename);
+            } else {
+                println!("Error: {}", res.status());
+            }
+        }
+        Err(err) => {
+            eprintln!("Failed to fetch data: {:?}", err);
+        }
+    }
+}
